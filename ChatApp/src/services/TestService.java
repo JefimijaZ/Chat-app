2885ac0e5e9b8789eb5c77207d62f46ce74b4dd4
jefimija <zivkovic.jefimijaa@gmail.com>
2018-04-26 16:24:27 +0200

    inital commit

---
+++ ChatApp/src/services/TestService.java
@@ -0,0 +1,27 @@
+package services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import message.JMSTopic;
+
+@Path("/demo")
+public class TestService {
+
+	/**
+	 * Testira REST sistem. URL izgleda ovako: <code>rest/demo/test</code>
+	 * 
+	 * @return Vraæa tekst da vidimo da sve radi.
+	 */
+	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test() {
+		new JMSTopic();
+		return "REST is working.";
+	}
+
+
+}
